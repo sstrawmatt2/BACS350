5a49a5a76471359e6374e8ce4c3f51631901ebd6
sstrawmatt2 <50216928+sstrawmatt2@users.noreply.github.com>
2020-09-09 13:50:10 -0600

    Still working on app2

--- app2/manage.py
+++ app2/manage.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app2.settings')
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app3.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -18,3 +20,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
