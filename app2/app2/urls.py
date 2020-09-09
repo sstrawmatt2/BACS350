9937e76b7ee944978b08b10310ccf093d12db020
sstrawmatt2 <50216928+sstrawmatt2@users.noreply.github.com>
2020-09-09 14:17:12 -0600

    Making changes on app2 again

--- app2/app2/urls.py
+++ app2/app2/urls.py
@@ -1,9 +1,9 @@
-from django.views.generic import TemplateView
 from django.urls import path
 
-class HomeView(TemplateView):
-    template_name='home.html'
+from pages.views import AboutView, HomeView
+
 
 urlpatterns = [
     path('', HomeView.as_view()),
+    path('about', AboutView.as_view()),
 ]
--- app2/pages/views.py
+++ app2/pages/views.py
@@ -1,3 +1,9 @@
-from pages.views import HomeView
 
-# Create your views here.
+from django.views.generic import TemplateView
+
+
+class HomeView(TemplateView):
+    template_name='home.html'
+
+class AboutView(TemplateView):
+    template_name='about.html'
