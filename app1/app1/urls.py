18a828bbd8e368e27f3261d1ff2c547637f4c18a
sstrawmatt2 <50216928+sstrawmatt2@users.noreply.github.com>
2020-09-08 19:58:27 -0600

    Update urls.py

--- app1/app1/urls.py
+++ app1/app1/urls.py
@@ -1,16 +1,11 @@
-
 from django.http import HttpResponse
 from django.urls import path
 
 
 def home_page_view(request):
-    return HttpResponse("<p>"+"HOME"+"</p>")
-
-def about_page_view(request):
-    return HttpResponse("<h1>ABOUT</h1>")
+    return HttpResponse("<h1>World's Simplest Website</h1>")
 
 
 urlpatterns = [
-    path('home', home_page_view),
-    path('about', about_page_view),
+    path('', home_page_view),
 ]
