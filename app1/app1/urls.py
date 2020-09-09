9c1d207f0233d75d7f1395ce1882401d04047542
sstrawmatt2 <50216928+sstrawmatt2@users.noreply.github.com>
2020-09-08 19:37:12 -0600

    Editing the urls.py

--- app1/app1/urls.py
+++ app1/app1/urls.py
@@ -1,21 +1,16 @@
-"""app1 URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
 
+
+def home_page_view(request):
+    return HttpResponse("<p>"+"HOME"+"</p>")
+
+def about_page_view(request):
+    return HttpResponse("<h1>ABOUT</h1>")
+
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('home', home_page_view),
+    path('about', about_page_view),
 ]
