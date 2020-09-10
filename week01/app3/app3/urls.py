2f757e268bedfdf756b720efb0c44d3a041592d4
sstrawmatt2 <50216928+sstrawmatt2@users.noreply.github.com>
2020-09-09 20:05:59 -0600

    changing up some settings

--- week01/app3/app3/urls.py
+++ week01/app3/app3/urls.py
@@ -1,7 +1,9 @@
 
-from django.contrib import admin
 from django.urls import path
 
+from pages.views import AboutView, HomeView
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', HomeView.as_view()),
+    path('about', AboutView.as_view()),
 ]
