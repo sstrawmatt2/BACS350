6441d151436b63a0acc35558784d5e303a5a993c
sstrawmatt2 <50216928+sstrawmatt2@users.noreply.github.com>
2020-09-10 15:24:44 -0600

    Working on demo5

--- week02/demo5/demo5/urls.py
+++ week02/demo5/demo5/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from django.views.generic import AboutView, HomeView
+
+from page.views import AboutView, HomeView, ProfileView
 
 urlpatterns = [
     path('about', AboutView.as_view()),
-    path('home', HomeSView.as_view()),
-    path('', TemplateView.as_view()),
+    path('home', HomeView.as_view()),
+    path('profile', ProfileView.as_view()),
+    path('', HomeView.as_view()),
 ]
