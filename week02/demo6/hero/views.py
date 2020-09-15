2eb7a3ee26f1ab5726c95fc7529e51e4f84f82cf
sstrawmatt2 <50216928+sstrawmatt2@users.noreply.github.com>
2020-09-14 20:07:58 -0600

    Working with heros/demo6 cont.

--- week02/demo6/hero/views.py
+++ week02/demo6/hero/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+class HulkView(TemplateView):
+    template_name = "hulk.html"
+    
+class WidowView(TemplateView):
+    template_name = "black_widow.html"
