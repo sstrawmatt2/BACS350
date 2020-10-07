4de575e1c81f7114afe0eb151dd970d107fb2f05
sstrawmatt2 <50216928+sstrawmatt2@users.noreply.github.com>
2020-10-07 15:05:55 -0600

    Making changes to home page

--- Superhero/hero/urls.py
+++ Superhero/hero/urls.py
@@ -1,10 +1,10 @@
 
-from hero.views import AboutPage, BasePage, HeroView, BatmanView, CapView, BartonView, HulkView, HomeView, DeadshotView
+from hero.views import AboutPage, BasePage, HeroView, BatmanView, CapView, BartonView, HulkView, HomePage, DeadshotView
 from django.urls import path
 
 urlpatterns = [
     path('', HeroView.as_view()),
-    path('home', HomeView.as_view(), name='home'),
+    path('home', HomeView.as_view()),
     path('about', AboutPage.as_view()),
     path('base', BasePage.as_view()),
     path('<str:identity>', HeroView.as_view()),
--- Superhero/hero/views.py
+++ Superhero/hero/views.py
@@ -6,7 +6,7 @@ class HeroView(TemplateView):
     template_name = "hero.html"
 
     def get_context_data(self, **kwargs):
-        id = kwargs.get('identity', 'Hulk')
+        id = kwargs.get('identity', 'Hero')
         return {'hero': id, 'css': '/static/hero.css'}
 
 class BasePage(TemplateView):
