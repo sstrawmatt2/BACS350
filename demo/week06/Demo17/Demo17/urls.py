33ad630cae9985aa59cd8610bfb1b342c6978545
sstrawmatt2 <50216928+sstrawmatt2@users.noreply.github.com>
2020-10-15 13:01:14 -0600

    working on demo17

--- demo/week06/Demo17/Demo17/urls.py
+++ demo/week06/Demo17/Demo17/urls.py
@@ -7,6 +7,11 @@ from django.views.generic import TemplateView
 class HeroView(TemplateView):
     template_name="hero.html"
     
+    def get_context_data(self, **kwargs):
+        return {
+            'title': 'My Hero View', 
+            'body': 'Heroes are awesome!',
+        }
     
     
 urlpatterns = [
