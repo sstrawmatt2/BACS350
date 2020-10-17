4e26ab220e09a00056d80fdf067e5fc3d822a2b8
sstrawmatt2 <50216928+sstrawmatt2@users.noreply.github.com>
2020-10-17 14:16:16 -0600

    still working on getting project 5 running

--- demo/week07/Demo18/hero/views.py
+++ demo/week07/Demo18/hero/views.py
@@ -1,12 +1,13 @@
 from django.views.generic import TemplateView
 
+from hero.models import Superhero
+
 class HeroView(TemplateView):
     template_name="hero.html"
     
     def get_context_data(self, **kwargs):
+        heroes = Superhero.objects.all()
         return {
-            'title': 'Superheroe Profile',
-            'name': 'Batman', 
-            'identity': 'Bruce Wayne',
-            
+            'title': 'Superhero Profile',
+            'heroes': heroes, 
         }
