2a1b3579e3e959e77e6b4bf0ea0bb9228cbe20da
sstrawmatt2 <50216928+sstrawmatt2@users.noreply.github.com>
2020-10-17 15:24:31 -0600

    Got project 5 working
    I was able to figure out the database for the heroes

--- Superhero/hero/urls.py
+++ Superhero/hero/urls.py
@@ -1,17 +1,23 @@
 
-from hero.views import AboutPage, BasePage, HeroView, BatmanView, CapView, BartonView, HulkView, HomePage, DeadshotView
+#from hero.views import AboutPage, BasePage, HeroView, BatmanView, CapView, BartonView, HulkView, HomePage, DeadshotView
 from django.urls import path
+from hero.views import HeroView
 
 urlpatterns = [
     path('', HeroView.as_view()),
-    path('home', HomePage.as_view(), name='home'),
-    path('about', AboutPage.as_view(), name = 'about'),
-    path('base', BasePage.as_view()),
-    path('<str:identity>', HeroView.as_view()),
-    path('batman', BatmanView.as_view(), name='batman'),
-    path('cap', CapView.as_view(), name='cap'),
-    path('barton', BartonView.as_view(), name='barton'),
-    path('hulk', HulkView.as_view(), name='hulk'),
-    path('deadshot', DeadshotView.as_view(), name='deadshot'),
-#    path('admin/', site.urls),
 ]
+
+#urlpatterns = [
+#    path('', HeroView.as_view()),
+#    path('home', HomePage.as_view(), name='home'),
+#    path('about', AboutPage.as_view(), name = 'about'),
+#    path('base', BasePage.as_view()),
+#    path('<str:identity>', HeroView.as_view()),
+#    path('batman', BatmanView.as_view(), name='batman'),
+#    path('cap', CapView.as_view(), name='cap'),
+#    path('barton', BartonView.as_view(), name='barton'),
+#    path('hulk', HulkView.as_view(), name='hulk'),
+#    path('deadshot', DeadshotView.as_view(), name='deadshot'),
+##    path('admin/', site.urls),
+#]
+
--- Superhero/hero/views.py
+++ Superhero/hero/views.py
@@ -1,35 +1,54 @@
 
 from django.views.generic import TemplateView
 
+from hero.models import Superhero
 
 class HeroView(TemplateView):
-    template_name = "hero.html"
-
+    template_name="hero.html"
+    
     def get_context_data(self, **kwargs):
-        id = kwargs.get('identity', 'Hero')
-        return {'hero': id, 'css': '/static/hero.css'}
-
-class BasePage(TemplateView):
-    template_name = "page_theme.html"
-
+        heroes = Superhero.objects.all()
+        return {
+            'title': 'Superhero Profile',
+            'heroes': heroes, 
+        }
 class HomePage(TemplateView):
     template_name = "home.html"
     
 class AboutPage(TemplateView):
     template_name = "about.html"
-    
-class BatmanView(TemplateView):
-    template_name = "batman.html"
 
-class CapView(TemplateView):
-    template_name = "cap.html"
-    
-class BartonView(TemplateView):
-    template_name = "barton.html"
+#
+#class HeroView(TemplateView):
+#    template_name = "hero.html"
+#
+#    def get_context_data(self, **kwargs):
+#        id = kwargs.get('identity', 'Hero')
+#        return {'hero': id, 'css': '/static/hero.css'}
+#
+#class BasePage(TemplateView):
+#    template_name = "page_theme.html"
+#
+#class HomePage(TemplateView):
+#    template_name = "home.html"
+#    
+#class AboutPage(TemplateView):
+#    template_name = "about.html"
+#    
+#class BatmanView(TemplateView):
+#    template_name = "batman.html"
+#
+#class CapView(TemplateView):
+#    template_name = "cap.html"
+#    
+#class BartonView(TemplateView):
+#    template_name = "barton.html"
+#
+#class HulkView(TemplateView):
+#    template_name = "hulk.html"
+#    
+#class DeadshotView(TemplateView):
+#    template_name = "deadshot.html"
 
-class HulkView(TemplateView):
-    template_name = "hulk.html"
-    
-class DeadshotView(TemplateView):
-    template_name = "deadshot.html"
+
 
