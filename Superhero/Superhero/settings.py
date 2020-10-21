8649d5fccda89e675afbe19d667803738c242556
sstrawmatt2 <50216928+sstrawmatt2@users.noreply.github.com>
2020-10-21 15:42:04 -0600

    Working on crispy forms

--- Superhero/Superhero/settings.py
+++ Superhero/Superhero/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hero',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
--- Superhero/hero/views.py
+++ Superhero/hero/views.py
@@ -17,6 +17,11 @@ class HeroEditView(UpdateView):
     template_name = "hero_add.html"
     model = Superhero
     fields = '__all__'
+    
+    def get_context_data(self, **kwargs):
+        kwargs = super(HeroEditView, self).get_context_data(**kwargs)
+        kwargs['edit'] = True
+        return kwargs
 
 class HeroListView(ListView):
     template_name = "hero_list.html"
