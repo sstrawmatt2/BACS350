8de2892be33cc1ba8e3b4f18551db2f2221488e8
sstrawmatt2 <50216928+sstrawmatt2@users.noreply.github.com>
2020-11-17 16:54:10 -0700

    Changes are being made

--- demo/week10/Demo27/workshop/views.py
+++ demo/week10/Demo27/workshop/views.py
@@ -1,5 +1,10 @@
 from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
-    template_name = "home.html"
+    template_name = 'home.html'
     
+    def get_context_data(self, **kwargs):
+        return {
+            'title': 'My About Page', 
+            'body': 'Once upon a time ...',
+        }
