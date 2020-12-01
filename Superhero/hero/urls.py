c6433dfd71e2ee0615f5a6364cbf57e8c73ca3c9
sstrawmatt2 <50216928+sstrawmatt2@users.noreply.github.com>
2020-12-01 12:35:52 -0700

    Making changes

--- Superhero/hero/urls.py
+++ Superhero/hero/urls.py
@@ -1,15 +1,16 @@
 from hero.views import HeroAddView, HeroDetailView, HeroEditView, HeroListView, HeroDeleteView, CardView, TemplateView
 from django.urls import path, include
+from django.contrib import admin
 
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="home.html")),
-    path('hero_list', HeroListView.as_view(), name='hero_list'),
-    path('hero_card', TemplateView.as_view(template_name="hero_card.html")),
+    path('', HeroListView.as_view(), name='hero_list'),
+    path('home', TemplateView.as_view(template_name='home')),
+    path('hero_card', TemplateView.as_view(template_name='hero_card')),
     path('markdown', TemplateView.as_view(template_name="markdown.html")),
     path('<int:pk>', HeroDetailView.as_view(), name='hero_detail'),
     path('add', HeroAddView.as_view(), name='hero_add'), 
     path('<int:pk>/', HeroEditView.as_view(), name='hero_edit'),
-    path('<int:pk>/', HeroDeleteView.as_view(), name='hero_delete'),
+    path('<int:pk>/delete', HeroDeleteView.as_view(), name='hero_delete'),
     path('', include('accounts.urls')),
 ] 
